162723c560e45b82cae169dce10739b8a3cc609e
Ilya Lebedev <bacherko@gmail.com>
2019-05-29 12:56:45 +0300

    fix: review fixes #71

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -2,8 +2,5 @@ import BigNum from 'bignumber.js';
 
 // eslint-disable-next-line
 export function calculateAmount({ amount, decs }) {
-  if (!decs) {
-    debugger;
-  }
   return new BigNum(amount).shiftedBy(-decs).toString();
 }
