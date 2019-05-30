076021a92f4fcc3c32c76bca0ccb205e57de06eb
Alexey Rekish <alexeyrekish@gmail.com>
2019-05-30 14:06:42 +0300

    fix: user statuses #84

--- src/components/userProfile/common/UserHeader/UserHeader.connect.js
+++ src/components/userProfile/common/UserHeader/UserHeader.connect.js
@@ -1,11 +1,30 @@
 import { connect } from 'react-redux';
 
+import { createSelector } from 'reselect';
+
+import { parsePayoutAmount } from 'utils/ParsersAndFormatters';
+import { dataSelector } from 'store/selectors/common';
 import { updateProfileMeta } from 'store/actions/cyberway/social';
 
 import UserHeader from './UserHeader';
 
 export default connect(
-  null,
+  createSelector(
+    [(state, props) => dataSelector(['wallet', props.profile.userId, 'vesting'])(state)],
+    vesting => {
+      let power = 0;
+
+      if (vesting && vesting.amount) {
+        const delegated = parsePayoutAmount(vesting.delegated) || 0;
+        const amount = parsePayoutAmount(vesting.amount) || 0;
+        power = amount - delegated;
+      }
+
+      return {
+        power,
+      };
+    }
+  ),
   {
     updateProfileMeta,
   }
