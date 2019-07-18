f0f1c71b3fa15ab7c581cd4baa3a4b8e6295b4ba
Alexey Elizarov <felex-ae@ya.ru>
2019-07-18 20:00:49 +0300

    fix: user balance #270

--- src/components/userProfile/common/AccountTokens/AccountTokens.connect.js
+++ src/components/userProfile/common/AccountTokens/AccountTokens.connect.js
@@ -2,6 +2,8 @@ import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 
 import { userLiquidBalanceSelector, userVestingBalanceSelector } from 'store/selectors/wallet';
+import { getBalance } from 'store/actions/gate';
+import { statusSelector } from 'store/selectors/common';
 
 import AccountTokens from './AccountTokens';
 
@@ -10,11 +12,16 @@ export default connect(
     [
       (state, props) => userLiquidBalanceSelector(props.userId)(state),
       (state, props) => userVestingBalanceSelector(props.userId)(state),
+      statusSelector(['wallet', 'isLoading']),
     ],
-    (liquid, vesting) => ({
+    (liquid, vesting, isLoading) => ({
       golos: liquid,
       power: vesting.total,
       powerDelegated: vesting.inDelegated,
+      isLoading,
     })
-  )
+  ),
+  {
+    getBalance,
+  }
 )(AccountTokens);
