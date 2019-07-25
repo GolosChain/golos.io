3bfa9cd9195e991a3b60a6c40b1062614b75053c
Alexey Elizarov <felex-ae@ya.ru>
2019-07-25 14:21:30 +0300

    fix: improve

--- src/store/actions/gate/content.js
+++ src/store/actions/gate/content.js
@@ -123,8 +123,6 @@ export const getNotifyMeta = ({ contentId } = {}) => {
       params,
       schema: contentMetaSchema,
     },
-    meta: {
-      ...params,
-    },
+    meta: params,
   };
 };
