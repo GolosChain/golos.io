981829265522c08f85393bc064b84cf6c7d86b29
Alexey Elizarov <felex-ae@ya.ru>
2019-08-07 19:02:22 +0300

    fix: wallet dublicates list #338

--- src/components/userProfile/wallet/vestings/VestingsList/VestingsList.js
+++ src/components/userProfile/wallet/vestings/VestingsList/VestingsList.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import styled from 'styled-components';
 import tt from 'counterpart';
 
@@ -34,10 +34,6 @@ export default function VestingsList({
   isHistoryEnd,
   getVestingHistory,
 }) {
-  useEffect(() => {
-    getVestingHistory({ userId, sequenceKey });
-  }, []);
-
   const onNeedLoadMore = useCallback(async () => {
     try {
       await getVestingHistory({ userId, sequenceKey });
