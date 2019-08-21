b8d8d538217999116c3bebb043a02ee95938fa08
b1acksun <tkachenko.igor88@gmail.com>
2019-08-21 09:46:14 +0300

    fix: about panel #342

--- src/containers/post/AboutPanel/AboutPanel.connect.js
+++ src/containers/post/AboutPanel/AboutPanel.connect.js
@@ -1,37 +1,16 @@
 import { connect } from 'react-redux';
-import { createSelector } from 'reselect';
 
-import { currentUsernameSelector } from 'store/selectors/auth';
+import { currentUserIdSelector } from 'store/selectors/auth';
 import { profileSelector } from 'store/selectors/common';
 
 import AboutPanel from './AboutPanel';
 
 export default connect(
-  // createSelector(
-  //   [authorSelector, currentPostSelector],
-  //   (author, post) => {
-  //     // set account join date
-  //     const transferFromSteemToGolosDate = '2016-09-29T12:00:00';
-  //     let { created } = author;
-  //
-  //     if (created && new Date(created) < new Date(transferFromSteemToGolosDate)) {
-  //       created = transferFromSteemToGolosDate;
-  //     }
-  //
-  //     return {
-  //       name: author.name,
-  //       account: author.account,
-  //       about: author.about,
-  //       url: post.url,
-  //       created,
-  //     };
-  //   }
-  // ),
   (state, { post }) => {
-    const currentUsername = currentUsernameSelector(state);
+    const currentUserId = currentUserIdSelector(state);
 
     return {
-      currentUsername,
+      currentUserId,
       profile: profileSelector(post.author)(state),
     };
   },
