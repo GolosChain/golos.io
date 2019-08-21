24f979dd79156f696dace0c3f41a18087a33ec17
Ilya Lebedev <bacherko@gmail.com>
2019-08-21 18:20:54 +0300

    fix: improve AccountNameInput #493

--- src/components/common/AccountNameInput/AccountNameInput.connect.js
+++ src/components/common/AccountNameInput/AccountNameInput.connect.js
@@ -1,11 +1,22 @@
 import { connect } from 'react-redux';
 
 import { suggestNames } from 'store/actions/gate';
+import { entitySelector } from 'store/selectors/common';
 
 import AccountNameInput from './AccountNameInput';
 
 export default connect(
-  null,
+  (state, props) => {
+    let targetUser;
+
+    if (props.value) {
+      targetUser = entitySelector('users', props.value)(state);
+    }
+
+    return {
+      username: targetUser?.username,
+    };
+  },
   {
     suggestNames,
   }
