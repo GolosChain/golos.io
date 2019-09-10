719c0ef02b9bbe62fcb22c2860a6c6107bf8fd0c
Ilya Lebedev <bacherko@gmail.com>
2019-09-10 17:24:45 +0300

    fix: delete unused code

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,24 +1,13 @@
 // sometimes it's impossible to use html tags to style coin name, hence usage of _UPPERCASE modifier
 export const APP_NAME = 'Голос';
 export const APP_NAME_LATIN = 'Golos';
-export const TITLE_SUFFIX = 'Golos.io';
-// sometimes APP_NAME is written in non-latin characters, but they are needed for technical purposes
-// ie. "Голос" > "Golos"
 export const APP_NAME_UP = 'GOLOS.io';
 export const APP_ICON = 'golos';
-// FIXME figure out best way to do this on both client and server from env
-// vars. client should read $STM_Config, server should read config package.
 export const APP_DOMAIN = 'golos.io';
 export const APP_URL = 'https://golos.io';
-export const LIQUID_TOKEN = 'Голос';
-
-export const CURRENCY_SIGN = '₽≈';
 
 // these are dealing with asset types, not displaying to client, rather sending data over websocket
 export const LIQUID_TICKER = 'GOLOS';
-export const VEST_TICKER = 'GESTS';
-export const DEBT_TICKER = 'GBG';
-export const DEBT_TOKEN_SHORT = 'GBG';
 
 // application settings
 export const DEFAULT_LOCALE = 'ru';
@@ -44,11 +33,11 @@ export const CURRENCIES = ['USD', 'RUB', 'EUR', LIQUID_TICKER];
 export const DEFAULT_CURRENCY = 'RUB';
 
 // meta info
-export const TWITTER_HANDLE = '@goloschain';
-export const SHARE_IMAGE = `https://${APP_DOMAIN}/images/golos-share.png`;
-export const TWITTER_SHARE_IMAGE = `https://${APP_DOMAIN}/images/golos-twshare.png`;
-export const SITE_DESCRIPTION =
-  'Голос - социальная сеть, построенная на публичном блокчейне. Вознаграждение пользователей осуществляется за счет дополнительной эмиссии токенов. При этом распределением токенов управляют текущие пользователи сети через голосование за лучший контент.';
+// export const TWITTER_HANDLE = '@goloschain';
+// export const SHARE_IMAGE = `https://${APP_DOMAIN}/images/golos-share.png`;
+// export const TWITTER_SHARE_IMAGE = `https://${APP_DOMAIN}/images/golos-twshare.png`;
+// export const SITE_DESCRIPTION =
+//   'Голос - социальная сеть, построенная на публичном блокчейне. Вознаграждение пользователей осуществляется за счет дополнительной эмиссии токенов. При этом распределением токенов управляют текущие пользователи сети через голосование за лучший контент.';
 
 // registration
 export const REGISTRATION_URL = 'https://reg.golos.io/';
@@ -57,58 +46,17 @@ export const REGISTRATION_URL = 'https://reg.golos.io/';
 export const SUPPORT_EMAIL = `support@${APP_DOMAIN}`;
 export const SUPPORT_EMAIL_2 = 'pr@golos.io';
 // ignore special tags, dev-tags, partners tags
-export const IGNORE_TEST_TAGS = ['test'];
 export const IGNORE_TAGS = ['bm-open', 'bm-ceh23', 'bm-tasks', 'bm-taskceh1'];
-export const PUBLIC_API = {
-  created: 'getDiscussionsByCreatedAsync',
-  recent: 'getDiscussionsByCreatedAsync',
-  hot: 'getDiscussionsByHotAsync',
-  trending: 'getDiscussionsByTrendingAsync',
-  promoted: 'getDiscussionsByPromotedAsync',
-  active: 'getDiscussionsByActiveAsync',
-  responses: 'getDiscussionsByChildrenAsync',
-  votes: 'getDiscussionsByVotesAsync',
-  cashout: 'getDiscussionsByCashoutAsync',
-  payout: 'getDiscussionsByPayoutAsync',
-  feed: 'getDiscussionsByFeedAsync',
-};
-export const ACCOUNT_OPERATIONS = [
-  'transfer_to_vesting',
-  'withdraw_vesting',
-  'interest',
-  'transfer',
-  'convert',
-  'fill_convert_request',
-  'transfer_to_savings',
-  'transfer_from_savings',
-  'cancel_transfer_from_savings',
-];
-
-export const SEO_TITLE = 'GOLOS.io Блоги';
 
 export const USER_GENDER = ['undefined', 'male', 'female'];
 
-export const SMS_SERVICES = {
-  default: '+46769438807',
-  '7': '+79169306359',
-  '77': '+77770076977',
-  '375': '+375292308770',
-  '380': '+380931777772',
-};
-
-export const ANDROID_APP_NAME = 'Golos.io';
 export const ANDROID_PACKAGE = 'io.golos.golos';
-export const ANDROID_URL_SCHEME = 'golosioapp';
 export const ANDROID_DEEP_LINK_DOMAIN = 'golos.io';
 export const TERMS_OF_SERVICE_URL = 'https://golos.io/legal/terms_of_service.pdf';
 export const GOLOS_EXPLORER = 'https://explorer.golos.io';
 export const WIKI_URL = 'https://wiki.golos.io/';
-export const MARKDOWN_STYLING_GUIDE_URL =
-  'https://golos.io/ru--golos/@on0tole/osnovy-oformleniya-postov-na-golose-polnyi-kurs-po-rabote-s-markdown';
 
 export const MIN_VOICE_POWER = 3;
-
-export const DEFAULT_VOTE_LIMIT = 10000;
 
 export const LEAVE_PAGE_WHITELIST_DOMAINS = [
   'golos.io',
@@ -143,8 +91,6 @@ export const AUCTION_REWARD_DESTINATION = {
   },
 };
 
-export const fbApp = null;
-export const readOnlyMode = false;
 export const imgHostingUrl = process.env.IMAGE_HOSTING_URL;
 export const imgProxyPrefix = `${process.env.IMAGE_HOSTING_URL}/proxy`;
 export const NSFW_IMAGE_URL = '/images/nsfw/nsfw.svg';
