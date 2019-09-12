7ecee86b06d46da4ad4e7c7edfba6f0b0ce74748
Alexey Elizarov <felex-ae@ya.ru>
2019-09-12 16:55:14 +0300

    fix: improve

--- src/store/reducers/status/postComments.js
+++ src/store/reducers/status/postComments.js
@@ -29,18 +29,6 @@ export default function(state = initialState, { type, payload, meta }) {
       return u.updateIn(postId, { ...initialPostCommentState, isLoading: true }, state);
     }
 
-    // if (meta.sequenceKey && meta.sequenceKey === state.sequenceKey) {
-    //   return {
-    //     ...state,
-    //     isLoading: true,
-    //   };
-    // }
-    //
-    // return {
-    //   ...initialState,
-    //   isLoading: true,
-    // };
-
     case FETCH_POST_COMMENTS_SUCCESS: {
       const postId = formatContentId({ userId: meta.userId, permlink: meta.permlink });
       let order;
@@ -51,21 +39,12 @@ export default function(state = initialState, { type, payload, meta }) {
       } else {
         order = payload.result.items;
       }
-      //
-      // return {
-      //   ...state,
-      //   isLoading: false,
-      //   order,
-      //   sequenceKey: payload.result.sequenceKey,
-      //   isEnd: payload.result.items.length < meta.limit,
-      // };
 
       return u.updateIn(
         postId,
         {
           isLoading: false,
           order,
-          // order: items => [...items, ...payload.result.items],
           sequenceKey: payload.result.sequenceKey || null,
           isEnd: payload.result.items.length < meta.limit,
         },
@@ -75,7 +54,6 @@ export default function(state = initialState, { type, payload, meta }) {
 
     case FETCH_POST_COMMENTS_ERROR: {
       const postId = formatContentId({ userId: meta.userId, permlink: meta.permlink });
-      // return { ...state, isLoading: false };
 
       return u.updateIn(postId, { isLoading: false }, state);
     }
