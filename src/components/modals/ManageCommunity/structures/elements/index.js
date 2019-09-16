f7f40b75f359c284b00e09dbcce427b990c5e2b5
Ilya Lebedev <bacherko@gmail.com>
2019-09-16 19:45:50 +0300

    fix: fix community settings (#644)

--- src/components/modals/ManageCommunity/structures/elements/index.js
+++ src/components/modals/ManageCommunity/structures/elements/index.js
@@ -25,7 +25,7 @@ export const DefaultText = styled.div`
 
 export const FieldSubTitle = styled.h3`
   display: block;
-  margin: 6px 0 4px;
+  margin: 8px 0 4px;
   line-height: 20px;
   font-size: 15px;
   font-weight: normal;
