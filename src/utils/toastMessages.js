cce48ad5886c411fd03bc8210a3d403638d0d9dc
Alexey Elizarov <felex-ae@ya.ru>
2019-09-17 17:41:36 +0300

    fix: refactoring toasts messages

--- src/utils/toastMessages.js
+++ src/utils/toastMessages.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console,no-param-reassign */
+import tt from 'counterpart';
 import ToastsManager from 'toasts-manager';
 
 import { normalizeCyberwayErrorMessage } from './errors';
@@ -32,6 +34,12 @@ export function displayError(title, err) {
 
   if (err) {
     message = normalizeCyberwayErrorMessage(err);
+
+    if (message.includes("Message doesn't exist in cashout window")) {
+      message = tt('chain_errors.cashout_window');
+    } else if (message.includes('incorrect proxy levels: grantor 1, agent 1')) {
+      message = tt('chain_errors.incorrect_delegate_proxy_level');
+    }
   }
 
   ToastsManager.error(`${prefix ? `${prefix} ` : ''}${message}`);
