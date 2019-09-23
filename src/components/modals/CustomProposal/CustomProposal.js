5a51030060ba5e7932112d0d30713fe90558081e
Ilya Lebedev <bacherko@gmail.com>
2019-09-23 18:16:16 +0300

    fix: review fixes

--- src/components/modals/CustomProposal/CustomProposal.js
+++ src/components/modals/CustomProposal/CustomProposal.js
@@ -10,7 +10,7 @@ import Button from 'components/golos-ui/Button';
 import { Input, Textarea } from 'components/golos-ui/Form';
 import Icon from 'components/golos-ui/Icon';
 import SplashLoader from 'components/golos-ui/SplashLoader';
-import DialogManager from '../../elements/common/DialogManager';
+import DialogManager from 'components/elements/common/DialogManager';
 
 // Фиксированный префикс нужен, чтобы можно было отличить обычные пропозалы от тех что созданы через форму.
 const PROPOSAL_PREFIX = 'lead';
