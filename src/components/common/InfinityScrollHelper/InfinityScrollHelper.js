3a3d6f6825f2cf79338bf7cc04cbaa1e279ea005
Alexey Elizarov <felex-ae@ya.ru>
2020-01-09 19:38:25 +0300

    fix: infinity scroll (#720)

--- src/components/common/InfinityScrollHelper/InfinityScrollHelper.js
+++ src/components/common/InfinityScrollHelper/InfinityScrollHelper.js
@@ -1,4 +1,4 @@
-import React, { PureComponent, createRef } from 'react';
+import React, { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { omit } from 'ramda';
@@ -6,7 +6,7 @@ import throttle from 'lodash.throttle';
 
 const Wrapper = styled.div``;
 
-export default class InfinityScrollHelper extends PureComponent {
+export default class InfinityScrollHelper extends Component {
   static propTypes = {
     disabled: PropTypes.bool,
     isDialog: PropTypes.bool,
@@ -21,7 +21,7 @@ export default class InfinityScrollHelper extends PureComponent {
 
   wrapperRef = createRef();
 
-  checkLoadMore = throttle(() => {
+  checkLoadMore = throttle(async () => {
     const { disabled, onNeedLoadMore } = this.props;
 
     if (disabled) {
@@ -32,20 +32,20 @@ export default class InfinityScrollHelper extends PureComponent {
     const { bottom } = wrapper.getBoundingClientRect();
 
     if (window.innerHeight * 1.5 > bottom) {
-      onNeedLoadMore();
+      await onNeedLoadMore();
+      this.checkLoadMore();
     }
   }, 500);
 
   componentDidMount() {
     const { isDialog } = this.props;
-    this.dialogWrapper = document.querySelector('.DialogManager__window-wrapper');
+    this.dialogWrapper = document.querySelector('.scroll-container');
 
     if (isDialog && this.dialogWrapper) {
       this.dialogWrapper.addEventListener('scroll', this.checkLoadMore);
     } else {
       window.addEventListener('scroll', this.checkLoadMore);
     }
-
     window.addEventListener('resize', this.checkLoadMore);
 
     this.delayedCheck = setTimeout(() => {
@@ -67,9 +67,9 @@ export default class InfinityScrollHelper extends PureComponent {
     }
 
     clearTimeout(this.delayedCheck);
+    this.checkLoadMore.cancel();
 
     window.removeEventListener('resize', this.checkLoadMore);
-    this.checkLoadMore.cancel();
   }
 
   render() {
