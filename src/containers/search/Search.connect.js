605435a135c60635960be84bdd96c0b97fbe93e1
Ilya Lebedev <bacherko@gmail.com>
2020-01-10 12:43:29 +0300

    fix: review fixes

--- src/containers/search/Search.connect.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-
-import { isSSRSelector } from 'store/selectors/ui';
-
-import Search from './Search';
-
-export default connect(state => ({
-  isSSR: isSSRSelector(state),
-}))(Search);
--- src/containers/search/index.js
+++ src/containers/search/index.js
@@ -1,1 +1,1 @@
-export { default } from './Search.connect';
+export { default } from './Search';
